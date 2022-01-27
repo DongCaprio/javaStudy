b5778133567eae699d840783db485886a9684c70
김동진 <dongjin403@naver.com>
2022-01-27 22:15:48 +0900

    ApplicationScope

---
+++ ApplicationScopeBeanJava/src/kr/co/softcampus/controller/TestController.java
@@ -0,0 +1,75 @@
+package kr.co.softcampus.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.softcampus.beans.DataBean1;
+import kr.co.softcampus.beans.DataBean2;
+import kr.co.softcampus.beans.DataBean3;
+import kr.co.softcampus.beans.DataBean4;
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	DataBean1 applicationBean1;
+
+	@Resource(name = "applicationBean2")
+	DataBean2 applicationBean2;
+	
+	//DataBean3부터는 Bean위에 @Component붙히고
+    //ServletAppContext에 @ComponentScan으로 패키지명 추가하고사용함
+	
+	@Autowired
+	DataBean3 applicationBean3;
+	
+	@Resource(name = "applicationBean4")
+	DataBean4 applicationBean4;
+	
+	@GetMapping("/test1")
+	public String test1() {
+		
+		applicationBean1.setData1("data1");
+		applicationBean1.setData2("data2");
+
+		applicationBean2.setData3("data3");
+		applicationBean2.setData4("data4");
+
+		applicationBean3.setData5("data5");
+		applicationBean3.setData6("data6");
+	
+		applicationBean4.setData7("data7");
+		applicationBean4.setData8("data8");
+		
+		return "test1";
+	}
+
+	@GetMapping("/result1")
+	public String result1(Model model) {
+		//applicationScoep는 바로 출력이 가능하다(다른 브라우져에서도 가능)
+		//but applicationScope영역에 저장이 되는것은 아니다!!!!!그러므로 출력하려면 model등을 사용해야 출력이 가능하다
+		System.out.println("applicationBean1.data1 : "+applicationBean1.getData1());
+		System.out.println("applicationBean1.data2 : "+applicationBean1.getData2());
+		
+		System.out.println("applicationBean2.data3 : "+applicationBean2.getData3());
+		System.out.println("applicationBean2.data4 : "+applicationBean2.getData4());
+		
+		System.out.println("applicationBean3.data5 : "+applicationBean3.getData5());
+		System.out.println("applicationBean3.data6 : "+applicationBean3.getData6());
+		
+		System.out.println("applicationBean4.data7 : "+applicationBean4.getData7());
+		System.out.println("applicationBean4.data8 : "+applicationBean4.getData8());
+		
+		//이걸해야지만 출력가능
+		model.addAttribute("applicationBean1", applicationBean1);
+		model.addAttribute("applicationBean2", applicationBean2);
+		
+		model.addAttribute("applicationBean3", applicationBean3);
+		model.addAttribute("applicationBean4", applicationBean4);
+		return "result1";
+	}
+}
